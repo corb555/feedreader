56c7374469145c5200701bfa602f5f90e5868259
corb555 <corb@aol.com>
2015-05-26 19:33:19 -0400

    Added description and instructions to readme Updated comments Fixed the length test for URL length and name length

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -14,90 +14,88 @@ $(function () {
      * feeds definitions, the allFeeds variable in our application.===
      */
     describe('RSS Feeds', function () {
-        /* This is our first test - it tests to make sure that the
+        /* Test to make sure that the
          * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
+         * empty. 
          */
         it('are defined', function () {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
+            expect(allFeeds).toBeDefined();   // All feeds defined
+            expect(allFeeds.length).not.toBe(0);  // Make sure allfeeds is not empty
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Loop through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
         it('must have a valid URL', function () {
             for (var id in allFeeds) {
-                expect(allFeeds[id].url).toBeDefined();
-                expect(allFeeds[id].url).length > 0;
-                expect(allFeeds[id].url.indexOf("http://")).toBe(0);
+                expect(allFeeds[id].url).toBeDefined();                 // URL is defined
+                expect(allFeeds[id].url.length).not.toBe(0);                    // URL is not empty
+                expect(allFeeds[id].url.indexOf("http://")).toBe(0);    // URL starts with http://
             }
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
+        /* Loop through each feed
+         * in the allFeeds object and ensure it has a name defined
          * and that the name is not empty.
          */
         it('must have a name', function () {
             for (var id in allFeeds) {
-                expect(allFeeds[id].name).toBeDefined();
-                expect(allFeeds[id].name).length > 0;
+                expect(allFeeds[id].name).toBeDefined();    // Name is defined
+                expect(allFeeds[id].name.length).not.toBe(0);       // Name is not empty
             }
         });
-
     });
 
 
     /* === test suite named "The menu" === */
     describe('The menu', function () {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /* Ensure the menu element is
+         * hidden by default. "menu-hidden" hides the menu and is toggled when user clicks
          */
         it('must be hidden by default', function () {
-            expect($('body').hasClass('menu-hidden')).toBe(true);
+            expect($('body').hasClass('menu-hidden')).toBe(true);  // If menu-hidden is present then menu is hidden
         });
 
-        /* TODO: Write a test that ensures the menu changes
-         * visibility when the menu icon is clicked. This test
-         * should have two expectations: does the menu display when
-         * clicked and does it hide when clicked again.
+        /* Ensure the menu changes
+         * visibility when the menu icon is clicked. 
+         * Tests both: 1. does the menu display when
+         * clicked and 2. does it hide when clicked again.
          */
         it('must change when menu clicked', function () {
             menu = $('.menu-icon-link');
 
-            menu.trigger('click');
-            expect($('body').hasClass('menu-hidden')).toBe(false);
+            // Click on menu
+            menu.click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);   // Make sure menu is not hidden
 
+            // Click again 
             menu.trigger('click');
-            expect($('body').hasClass('menu-hidden')).toBe(true);
+            expect($('body').hasClass('menu-hidden')).toBe(true);   // Make sure menu is now hidden
         });
     });
 
     /* === test suite named "Initial Entries" === */
     describe('Initial Entries', function () {
 
-        /* TODO: Write a test that ensures when the loadFeed
+        /* TODO: Ensure that when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test wil require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
+         * loadFeed() is asynchronous so this test uses
+         * Jasmine's beforeEach and asynchronous done() function.
          */
         beforeEach(function (done) {
+            // Loadfeed 0 and wait for it to complete
             loadFeed(0, function () {
                 done();
             });
         });
 
         it('must create at least a single .entry', function (done) {
+                // Verify that there is now an entry link
             expect($('.entry-link').length).toBeGreaterThan(0);
             done();
         });
@@ -106,9 +104,9 @@ $(function () {
     /* === test suite named "New Feed Selection ==="*/
     describe('New Feed Selection', function () {
         var oldHeadlines, newHeadlines;
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* Ensure that when a new feed is loaded
          * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
+         * Uses "done", since loadFeed() is asynchronous.
          */
         beforeEach(function (done) {
             // Save original headlines - from H2 tags
@@ -120,7 +118,10 @@ $(function () {
         });
 
         it('must change content', function (done) {
+            // Save new headlines
             newHeadlines = $('H2').text();
+            
+            // Verify that new headlines don't match old headlines
             expect(newHeadlines).not.toBe(oldHeadlines);
             done();
         });
